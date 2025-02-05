2ab7486098345eb39234d0336b0ce792e0a8c3bb
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-02-05 21:21:12 +0100

    Added Sign In screen

--- mungesat-front-app/src/components/SignForm.jsx
+++ mungesat-front-app/src/components/SignForm.jsx
@@ -4,8 +4,10 @@ import manStanding from "../assets/images/manStanding.svg";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 import "../styles/screens/signForm.css";
+
 function SignForm() {
   const [showPassword, setShowPassword] = useState(false);
+  const [onSignIn, setOnSignIn] = useState(true);
 
   const togglePassword = () => {
     setShowPassword(!showPassword);
@@ -26,21 +28,28 @@ function SignForm() {
       <div className="sign-text d-flex w-75">
         <div className="d-flex flex-column gap-2">
           <h1 className="fw-bold">
-            Sign in to <br />{" "}
+            {onSignIn ? " Sign in" : "Sign up"} to <br />{" "}
             <span className="fw-normal">eMungesat is simply</span>
           </h1>
-          <p className="mb-0"> If you don’t have an account register</p>
+          <p className="mb-0">
+            {" "}
+            If you {onSignIn ? "don’t have" : "have"} an account{" "}
+            {onSignIn ? "register" : "sign in"}
+          </p>
           <p>
             You can{" "}
-            <a href="#" className="register-here fw-bold text-decoration-none">
-              Register here!
-            </a>
+            <span
+              onClick={() => setOnSignIn(!onSignIn)}
+              className="register-here fw-bold text-decoration-none"
+            >
+              {onSignIn ? "Register " : "Sign In"} here!
+            </span>
           </p>
         </div>
         <img src={manStanding} alt="Man Standing" />
       </div>
       <div className="sign-form d-flex flex-column gap-4 w-50">
-        <h2 className="mb-4 fw-semibold">Sign in</h2>
+        <h2 className="mb-4 fw-semibold">{onSignIn ? "Sign in" : "Sign up"}</h2>
         <form className="w-75 d-flex flex-column gap-4">
           <input
             className="border-0 px-4 rounded-3"
@@ -65,9 +74,30 @@ function SignForm() {
               />
             )}
           </div>
-          <a className="forgot-p text-end text-decoration-none">
-            Forgot password?
-          </a>
+          {onSignIn ? (
+            <a className="forgot-p text-end text-decoration-none">
+              Forgot password?
+            </a>
+          ) : (
+            <div className="d-flex position-relative">
+              <input
+                className="border-0 px-4 rounded-3 w-100 pe-5"
+                type={showPassword ? "text" : "password"}
+                placeholder="Confirm Password"
+              />
+              {!showPassword ? (
+                <FaEyeSlash
+                  onClick={togglePassword}
+                  className="eye position-absolute top-50 translate-middle-y end-0 me-3"
+                />
+              ) : (
+                <FaEye
+                  onClick={togglePassword}
+                  className="eye position-absolute top-50 translate-middle-y end-0 me-3"
+                />
+              )}
+            </div>
+          )}
           <button className="sign-btn border-0 rounded-3 fw-semibold mt-3">
             Sign In
           </button>

ea47fc9f66493f2afdedf692239b469ac29a68e5
FatlumG <167577293+FatlumG@users.noreply.github.com>
2025-02-11 20:45:24 +0100

    Added register form based in DTOs

--- mungesat-front-app/src/App.jsx
+++ mungesat-front-app/src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignForm from "./screens/SignForm";
 import Dashboard from "./screens/Dashboard";
@@ -8,23 +9,29 @@ import ClassAdd from "./screens/ClassAdd";
 import Student from "./screens/Student";
 import StudentAdd from "./screens/StudentAdd";
 import Missings from "./screens/Missings";
-import Profile from "./screens/Profile";
 import Settings from "./screens/Settings";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignForm />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/teacher" element={<Teacher />} />
-        <Route path="/teacher-add" element={<TeacherAdd />} />
-        <Route path="/class" element={<Class />} />
-        <Route path="/class-add" element={<ClassAdd />} />
-        <Route path="/student" element={<Student />} />
-        <Route path="/student-add" element={<StudentAdd />} />
-        <Route path="/missings" element={<Missings />} />
-        <Route path="/settings" element={<Settings />} />
+        {isLoggedIn ? (
+          <>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/teacher" element={<Teacher />} />
+            <Route path="/teacher-add" element={<TeacherAdd />} />
+            <Route path="/class" element={<Class />} />
+            <Route path="/class-add" element={<ClassAdd />} />
+            <Route path="/student" element={<Student />} />
+            <Route path="/student-add" element={<StudentAdd />} />
+            <Route path="/missings" element={<Missings />} />
+            <Route path="/settings" element={<Settings />} />
+          </>
+        ) : (
+          <Route path="/" element={<SignForm />} />
+        )}
       </Routes>
     </Router>
   );
